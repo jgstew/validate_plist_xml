558b631de2874c8760f1ada55377cab07bc6536f
James Stewart <james.stewart@hcl.com>
2021-03-12 17:30:03 -0500

    pylint run fixes

--- src/validate_plist_xml/__main__.py
+++ src/validate_plist_xml/__main__.py
@@ -2,6 +2,6 @@
 To run this script as a module
 """
 # https://stackoverflow.com/questions/4042905/what-is-main-py
-from validate_plist_xml import main
+from validate_plist_xml import main  # pylint: disable=no-name-in-module
 
 main()
