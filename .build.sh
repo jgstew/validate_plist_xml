6a0cbec8b791bd840fd65039bbcf18dc65d7367f
James Stewart <james.stewart@hcl.com>
2021-03-13 16:46:12 -0500

    adding comments to build script

--- .build.sh
+++ .build.sh
@@ -1,8 +1,16 @@
 # to run: `bash .build.sh`
+echo "-- run build and publish to public pypi.org pip --"
 
 # delete old dist files
+echo "-- deleting old dist files --"
 rm dist/*
+
 # build
+echo "-- run build --"
 python3 -m build
-# publish new version to pip
+
+# publish new version to pip - requires configured creds
+echo "-- publishing --"
 python3 -m twine upload dist/*
+
+echo "-- finished --"
