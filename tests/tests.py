4cf23a55d7c09c9685208a150cecd42ae32503c9
James Stewart <james.stewart@hcl.com>
2021-03-21 16:22:27 -0400

    changing testing method slightly

--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,24 @@
 """test validate_plist_xml"""
 
+import argparse
 import os.path
-import site
+#import site
 import sys
 
-# https://stackoverflow.com/questions/34846584/whats-the-recommended-way-to-import-unittest-or-unittest2-depending-on-pyth/66616071
-# try:
-#    import unittest2 as unittest
-# except ImportError:
-#    import unittest
+# check for --test_pip arg
+parser = argparse.ArgumentParser()
+parser.add_argument("--test_pip", help="to test package installed with pip",
+                    action="store_true")
+args = parser.parse_args()
 
-# add module folder to import paths
-site.addsitedir(
-    os.path.join(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__))) , "src")
-)
+if not args.test_pip:
+    # add module folder to import paths for testing local src
+    sys.path.append(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src")
+    )
+    # reverse the order so we make sure to get the local src module, not pip package
+    sys.path.reverse()
 
 import validate_plist_xml  # pylint: disable=import-error,wrong-import-position
 
