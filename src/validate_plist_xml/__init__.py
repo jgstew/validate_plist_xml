edfc1895b26a17afac6e55c29fad1ebdcef6aef3
jgstew <james@jgstew.com>
2021-07-29 12:48:28 -0400

    change where the VERSION info is read from.

--- src/validate_plist_xml/__init__.py
+++ src/validate_plist_xml/__init__.py
@@ -1,4 +1,25 @@
 """
-To support running this directly
+To support validate_plist_xml as module
 """
+import os
+
 from .validate_plist_xml import main
+
+def _init_get_version():
+    """
+    This function gets the contents of VERSION.txt
+    - Get directory of this file __init__.py
+    - Get `VERSION.txt` path in same directory
+    - Read first line of VERSION.txt
+    - Trim whitespace characters from the first line
+    """
+    init_dir = os.path.dirname(os.path.realpath(__file__))
+    ver_file = os.path.join(init_dir, 'VERSION.txt')
+    with open(ver_file) as f:
+        first_line = f.readline().strip()
+    return first_line
+
+# read by `python .\setup.py --version`
+# read in `setup.cfg` by `version = attr: src.validate_plist_xml.__version__`
+__version__ = _init_get_version()
+VERSION = __version__
