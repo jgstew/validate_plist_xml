652db71a3aab06fc34cbce4d9f355211c28b8db0
jgstew <james@jgstew.com>
2021-07-29 12:54:46 -0400

    fixes for:isort, black, pylint, flake8

--- src/validate_plist_xml/__init__.py
+++ src/validate_plist_xml/__init__.py
@@ -5,6 +5,7 @@ import os
 
 from .validate_plist_xml import main
 
+
 def _init_get_version():
     """
     This function gets the contents of VERSION.txt
@@ -13,11 +14,12 @@ def _init_get_version():
     - Read first line of VERSION.txt
     - Trim whitespace characters from the first line
     """
-    init_dir = os.path.dirname(os.path.realpath(__file__))
-    ver_file = os.path.join(init_dir, 'VERSION.txt')
-    with open(ver_file) as f:
-        first_line = f.readline().strip()
+    init_dir_path = os.path.dirname(os.path.realpath(__file__))
+    ver_file_path = os.path.join(init_dir_path, "VERSION.txt")
+    with open(ver_file_path) as ver_file_obj:
+        first_line = ver_file_obj.readline().strip()
     return first_line
+
 
 # read by `python .\setup.py --version`
 # read in `setup.cfg` by `version = attr: src.validate_plist_xml.__version__`
