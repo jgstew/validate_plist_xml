0449e643d34d8f0db6d6961bd6dbf04d9f05af31
jgstew <james@jgstew.com>
2021-07-29 15:37:51 -0400

    change single source version info

--- src/validate_plist_xml/__init__.py
+++ src/validate_plist_xml/__init__.py
@@ -1,27 +1,7 @@
 """
 To support validate_plist_xml as module
 """
-import os
 
 from .validate_plist_xml import main
 
-
-def _init_get_version():
-    """
-    This function gets the contents of VERSION.txt
-    - Get directory of this file __init__.py
-    - Get `VERSION.txt` path in same directory
-    - Read first line of VERSION.txt
-    - Trim whitespace characters from the first line
-    """
-    init_dir_path = os.path.dirname(os.path.realpath(__file__))
-    ver_file_path = os.path.join(init_dir_path, "VERSION.txt")
-    with open(ver_file_path) as ver_file_obj:
-        first_line = ver_file_obj.readline().strip()
-    return first_line
-
-
-# read by `python .\setup.py --version`
-# read in `setup.cfg` by `version = attr: src.validate_plist_xml.__version__`
-__version__ = _init_get_version()
-VERSION = __version__
+__version__ = "1.0.2"
